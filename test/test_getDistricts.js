a5484275b8d38761a47292070efffc0f67ee3035
Chris Edwards <chrisedwards@Chriss-MacBook-Pro.local>
2016-12-19 19:44:29 -0500

    separating tests into files to match their functions

---
+++ test/test_getDistricts.js
@@ -0,0 +1,36 @@
+
+import { beforeEach, afterEach, describe, it } from 'mocha';
+import chai, { expect } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import request from 'request-promise';
+import sinon from 'sinon';
+
+import mock from './mock/github/unitedstates/districts/2016.json';
+import getDistricts from '../src/github/unitedstates/getDistricts';
+
+
+chai.use(chaiAsPromised);
+chai.config.includeStack = true;
+
+describe('#getDistricts', () => {
+    beforeEach(done => {
+        sinon
+            .stub(request, 'get')
+            .returns(Promise.resolve({body: mock}));
+        done();
+    });
+
+    afterEach((done) => {
+        request.get.restore();
+        done();
+    });
+
+    it('returns a list of all congressional districts', () => {
+        return (expect(getDistricts()))
+            .to.eventually
+            .be.an('array')
+            .and.have.length.of(435)
+            .and.include('CA-22');
+    });
+});
+
--- test/test_getDistrictsByState.js
+++ test/test_getDistrictsByState.js
@@ -6,33 +6,10 @@ import request from 'request-promise';
 import sinon from 'sinon';
 
 import mock from './mock/github/unitedstates/districts/2016.json';
-import getDistricts from '../src/github/unitedstates/getDistricts';
 import getDistrictsByState, { NO_DISTRICTS_FOUND } from '../src/github/unitedstates/getDistrictsByState';
 
 chai.use(chaiAsPromised);
 chai.config.includeStack = true;
-
-describe('#getDistricts', () => {
-    beforeEach(done => {
-        sinon
-            .stub(request, 'get')
-            .returns(Promise.resolve({body: mock}));
-        done();
-    });
-
-    afterEach((done) => {
-        request.get.restore();
-        done();
-    });
-
-    it('returns a list of all congressional districts', () => {
-        return (expect(getDistricts()))
-            .to.eventually
-            .be.an('array')
-            .and.have.length.of(435)
-            .and.include('CA-22');
-    });
-});
 
 describe('#getDistrictsByState', () => {
 
