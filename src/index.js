2ff3bafac8c7c29f4e5ae3b0e70e3aebbdc0c556
Chris Edwards <chrisedwards@Chriss-MacBook-Pro.local>
2016-12-20 17:45:00 -0500

    exporting getDistrictShape in entry file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 
 import getDistricts from './github/unitedstates/getDistricts';
 import getDistrictsByState from './github/unitedstates/getDistrictsByState';
-
+import getDistrictShape from './github/unitedstates/getDistrictShape';
 
 export default {
     getDistricts,
-    getDistrictsByState
+    getDistrictsByState,
+    getDistrictShape
 };
