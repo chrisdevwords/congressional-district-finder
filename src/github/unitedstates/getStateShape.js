e6dbbf6d8844d1c267584683ce2368bad8465067
Chris Edwards <chrisedwards@Chriss-MacBook-Pro.local>
2016-12-22 08:15:03 -0500

    adding function to getStateShape from github/unitedstates

---
+++ src/github/unitedstates/getStateShape.js
@@ -0,0 +1,21 @@
+
+import request from 'request-promise';
+import flattenMultiPolygon from './flattenMultiPolygon';
+
+
+export const endpoint = st =>
+    `https://theunitedstates.io/districts/cds/2016/${st}/shape.geojson`;
+
+export function parseStateShape({ coordinates }) {
+    const polygons = flattenMultiPolygon(coordinates);
+    return { polygons }
+}
+
+export default function getStateShape(st) {
+    const options = {
+        uri: endpoint(st),
+        json: true
+    };
+    return request.get(options)
+        .then(parseStateShape);
+}
