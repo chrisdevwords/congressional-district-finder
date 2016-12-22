45773f5bee31b0189f1ed8e8f3db901337ccdf2c
Chris Edwards <chrisedwards@Chriss-MacBook-Pro.local>
2016-12-22 10:34:30 -0500

    adding checkLatLngInDistrict function

---
+++ src/github/unitedstates/checkLatLngInDistrict.js
@@ -0,0 +1,22 @@
+
+import getDistrictShape from './getDistrictShape';
+import isLatLngInMultiPolygon from '../../geolib/isLatLngInMultiPolygon';
+
+
+function checkLatLngInDistrict(lat, lng, districtId) {
+    return getDistrictShape(districtId)
+        .then((district) => {
+
+            const { polygons } = district;
+            const isMatched = isLatLngInMultiPolygon(lat, lng, polygons);
+            return {
+                isMatched,
+                districtId,
+                district,
+                latitude: lat,
+                longitude: lng
+            };
+        });
+}
+
+export default checkLatLngInDistrict;
