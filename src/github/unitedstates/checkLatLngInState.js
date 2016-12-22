223891f6ae136b179157c5c200ab7b2b85329559
Chris Edwards <chrisedwards@Chriss-MacBook-Pro.local>
2016-12-22 11:06:26 -0500

    adding function to detect if lat lng is in state

---
+++ src/github/unitedstates/checkLatLngInState.js
@@ -0,0 +1,20 @@
+
+import getStateShape from './getStateShape';
+import isLatLngInMultiPolygon from '../../geolib/isLatLngInMultiPolygon';
+
+
+function checkLatLngInState(lat, lng, stateId) {
+    return getStateShape(stateId)
+        .then(({ polygons }) => {
+
+            const isMatched = isLatLngInMultiPolygon(lat, lng, polygons);
+            return {
+                isMatched,
+                stateId,
+                latitude: lat,
+                longitude: lng
+            };
+        });
+}
+
+export default checkLatLngInState;
