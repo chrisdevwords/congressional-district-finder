cb1aee52d42bb5c4a3956217e0ebecdbf716eef5
Chris Edwards <chrisdevwords@yahoo.com>
2016-12-23 12:18:57 -0500

    page scraper can find and parse district codes in a successful result

--- src/house/gov/getDistrictsInZip.js
+++ src/house/gov/getDistrictsInZip.js
@@ -2,21 +2,50 @@
 import request from 'request-promise';
 
 
+export const INVALID_ZIP = zip =>
+    `Invalid zip code: "${zip}".`;
+
+export const NO_RESULTS_ZIP = zip =>
+    `No results for zip code: "${zip}".`;
+
+export const INVALID_DISTRICT_CODE_STRING = val =>
+    `District code substring: "${val}" is an invalid format.`;
+
 export const endpoint = zip =>
     `http://ziplook.house.gov/htbin/findrep?ZIP=${zip}`;
 
 export function parseDistrictCode(val) {
-    return val;
+
+    if (!val) {
+        throw new Error(INVALID_DISTRICT_CODE_STRING(val));
+    }
+
+    const num = val.match(/\d+/g);
+    const st = val.substring(1, 3);
+
+    if (!num || !st ||
+        st.length !== 2 ||
+        num.length !== 1 ||
+        isNaN(num[0]) ||
+        num[0].length > 2
+    ) {
+        throw new Error(INVALID_DISTRICT_CODE_STRING(val));
+    }
+
+    return `${st}-${Number(num)}`;
 }
 
-export function parsePage(body) {
-    // todo matching district codes are in an array in inline js
-    //console.log(body);
-    return body;
+export function scrapePage(body) {
+    const startStr = 'districts=[';
+    const start = body.indexOf(startStr) + startStr.length;
+    const end = body.indexOf('];', start);
+    const data = body.substring(start, end).split(',');
+    return data
+        .map(parseDistrictCode);
 }
 
 export default function getDistrictsInZip(zip) {
     return request
         .get({ uri: endpoint(zip) })
-        .then(parsePage);
+        .then(scrapePage);
 }
--- test/house/gov/test_getDistrictsInZip.js
+++ test/house/gov/test_getDistrictsInZip.js
@@ -8,7 +8,11 @@ import request from 'request-promise';
 import sinon from 'sinon';
 
 import getDistrictsInZip, {
-    parsePage
+    scrapePage,
+    parseDistrictCode,
+    INVALID_DISTRICT_CODE_STRING,
+    INVALID_ZIP,
+    NO_RESULTS_ZIP,
 }  from '../../../src/house/gov/getDistrictsInZip';
 
 const { beforeEach, afterEach, describe, it } = mocha;
@@ -33,85 +37,144 @@ const openMock = (options) => {
     });
 };
 
-describe('#getDistrictsInZip', () => {
+describe('house.gov pagescraper', () =>{
 
-    context('with a valid zip code', () => {
+    describe('#getDistrictsInZip', () => {
 
-        beforeEach((done) => {
+        context('with a valid zip code', () => {
 
-            sinon
-                .stub(request, 'get', openMock);
-            done();
+            beforeEach((done) => {
+                sinon
+                    .stub(request, 'get', openMock);
+                done();
 
+            });
+
+            afterEach((done) => {
+                request.get.restore();
+                done();
+            });
+
+            it('can parse a single district', (done) => {
+                getDistrictsInZip(15101)
+                    .then((data)=> {
+                        expect(data[0]).to.eq('PA-12');
+                        expect(data.length).to.eq(1);
+                        done();
+                    }).catch(done);
+            });
+
+            it('can parse multiple districts', (done) => {
+                getDistrictsInZip(92373)
+                    .then((data)=> {
+                        expect(data).to.be.an('array');
+                        expect(data.length).to.eq(4);
+                        expect(data[0]).to.eq('CA-8');
+                        expect(data[3]).to.eq('CA-41');
+                        done();
+                    }).catch(done);
+            });
         });
 
-        afterEach((done) => {
-            request.get.restore();
-            done();
-        });
+        context('with an invalid zip code', () => {
 
-        it('can parse a single district', (done) => {
-            getDistrictsInZip(92373)
-                .then((data)=> {
-                    expect(data).to.be.a.string;
-                    //done();
-                    done(Error('Test not complete'));
-                }).catch(done);
-        });
+            beforeEach((done) => {
+                sinon
+                    .stub(request, 'get', openMock);
+                done();
+            });
 
-        it.skip('can parse multiple districts', (done) => {
-            done('Test not complete');
-        });
-    });
+            afterEach((done) => {
+                request.get.restore();
+                done();
+            });
 
-    context('with an invalid zip code', () => {
+            it.skip('rejects with an invalid zipcode error without making a request', (done) => {
+                // valdiate zip code with regex
+                // check before making http request
+                // spy on request and verify it's not called
+                done(Error('Test not complete'));
+            });
 
-        beforeEach((done) => {
-            sinon
-                .stub(request, 'get')
-                .returns(Promise.resolve({ }));
-            done();
-        });
+            it.skip('rejects with an invalid zipcode error', (done) => {
+                done(Error('Test not complete'));
+            });
 
-        afterEach((done) => {
-            request.get.restore();
-            done();
-        });
-
-        it.skip('rejects with an invalid zipcode error without making a request', (done) => {
-            // valdiate zip code with regex
-            // check before making http request
-            // spy on request and verify it's not called
-            done(Error('Test not complete'));
-        });
-
-        it.skip('rejects with an invalid zipcode error', (done) => {
-            done(Error('Test not complete'));
-        });
-
-        it.skip('rejects with a no districts found error', (done) => {
-            done(Error('Test not complete'));
-        });
-    });
-});
-
-describe('#parsePage', () => {
-
-    context('with a successful result', () => {
-        it.skip('can extract a single district code', (done) => {
-            done(Error('Test not complete'));
-        });
-        it.skip('can extract multiple district codes', (done) => {
-            done(Error('Test not complete'));
+            it.skip('rejects with a no districts found error', (done) => {
+                done(Error('Test not complete'));
+            });
         });
     });
 
-    context('with a unsuccessful result', () => {
-        it.skip('can parse a no results message', (done) => {
-            done(Error('Test not complete'));
+    describe('#scrapePage', () => {
+
+        context('with a successful result', () => {
+            it.skip('can extract a single district code', (done) => {
+                done(Error('Test not complete'));
+            });
+            it.skip('can extract multiple district codes', (done) => {
+                done(Error('Test not complete'));
+            });
         });
-        it.skip('can parse an invalid zip code error', (done) => {
-            done(Error('Test not complete'));
+
+        context('with a unsuccessful result', () => {
+            it.skip('can parse a no results message', (done) => {
+                done(Error('Test not complete'));
+            });
+            it.skip('can parse an invalid zip code error', (done) => {
+                done(Error('Test not complete'));
+            });
+        });
+    });
+
+    describe('#parseDistrictCode', () => {
+        it('can parse a single digit code', (done) => {
+            expect(parseDistrictCode('"NY09"'))
+                .to.eq('NY-9');
+            done();
+        });
+
+        it('can parse a double digit code', (done) => {
+            expect(parseDistrictCode('"TX11"'))
+                .to.eq('TX-11');
+            done();
+        });
+
+        it('throws an informative error if expected js code changes', (done) => {
+            const jsChange = " new DistrictCodes(11211)";
+            expect(() => {
+                parseDistrictCode(jsChange)
+            }).to.throw(INVALID_DISTRICT_CODE_STRING(jsChange));
+            done();
+        });
+
+        it('throws an informative error if expected js code changes', (done) => {
+            const jsChange = "codes.pop()";
+            expect(() => {
+                parseDistrictCode(jsChange)
+            }).to.throw(INVALID_DISTRICT_CODE_STRING(jsChange));
+            done();
+        });
+
+        it('throws an informative error if passed an empty string ', (done) => {
+            expect(() => {
+                parseDistrictCode("")
+            }).to.throw(INVALID_DISTRICT_CODE_STRING(""));
+            done();
+        });
+
+        it('throws an informative error if passed undefined ', (done) => {
+            expect(() => {
+                parseDistrictCode()
+            }).to.throw(INVALID_DISTRICT_CODE_STRING());
+            done();
+        });
+
+        it('throws an informative error if passed null', (done) => {
+            expect(() => {
+                parseDistrictCode(null)
+            }).to.throw(INVALID_DISTRICT_CODE_STRING(null));
+            done();
         });
     });
 });
