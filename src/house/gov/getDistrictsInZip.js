4669b4f03269f9f0a6992c6c98a095e5c2f18f93
Chris Edwards <chrisedwards@Chriss-MacBook-Pro.local>
2016-12-23 09:07:02 -0500

    stubbing out page scraper for house zip code to district fetch/parser

---
+++ src/house/gov/getDistrictsInZip.js
@@ -0,0 +1,22 @@
+
+import request from 'request-promise';
+
+
+export const endpoint = zip =>
+    `http://ziplook.house.gov/htbin/findrep?ZIP=${zip}`;
+
+export function parseDistrictCode(val) {
+    return val;
+}
+
+export function parsePage(body) {
+    // todo matching district codes are in an array in inline js
+    console.log(body);
+    return [];
+}
+
+export default function getDistrictsInZip(zip) {
+    return request
+        .get({ uri: endpoint(zip) })
+        .then(parsePage);
+}
