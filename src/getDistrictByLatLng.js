8fc5de4cd8affbab4818a4c521083008db5d6eea
Chris Edwards <chrisdevwords@yahoo.com>
2016-12-26 15:49:31 -0500

    adding getDistrictByLatLng function

---
+++ src/getDistrictByLatLng.js
@@ -0,0 +1,33 @@
+
+import getStateZipFromLatLng from
+    './google/maps/geocode/getStateZipFromLatLng';
+import getDistrictsInZip from './house/gov/getDistrictsInZip'
+import checkLatLngInDistricts from './checkLatLngInDistricts';
+
+export const OUTSIDE_US = (lat, lng, country) =>
+    `The specified latitude: ${lat} and ` +
+    `longitude: ${lng} are for the country: ${country}, ` +
+    'To Find a Congressional District, please provide coordinates in the US.';
+
+
+function getDistrictByLatLng(latitude, longitude) {
+
+    return getStateZipFromLatLng(latitude, longitude)
+        .then((data) => {
+            const { country } = data;
+            if (country !== 'US') {
+                return Promise.reject({
+                    statusCode: 404,
+                    // eslint-disable-next-line babel/new-cap
+                    message: OUTSIDE_US(latitude, longitude, country)
+                });
+            }
+            return data;
+        })
+        .then(({ zip }) => getDistrictsInZip(zip))
+        .then(districts =>
+            checkLatLngInDistricts(latitude, longitude, districts)
+        );
+}
+
+export default getDistrictByLatLng;
---
+++ test/test_getDistrictByLatLng.js
@@ -0,0 +1,131 @@
+
+
+import mocha from 'mocha';
+import chai from 'chai';
+import request from 'request-promise';
+import sinon from 'sinon';
+
+import { INVALID_REQUEST } from '../src/google/maps/geocode/getStateZipFromLatLng';
+import getDistrictByLatLng, {
+    OUTSIDE_US,
+} from '../src/getDistrictByLatLng';
+
+const { describe, it } = mocha;
+const { expect, config } = chai;
+
+config.includeStack = true;
+
+
+describe('#getDistrictByLatLng', () => {
+
+    context('with latitude and longitude', () => {
+        it('can find a district in Westerville, OH', (done) => {
+
+            const lat = 40.128072;
+            const lng = -82.9184697;
+
+            getDistrictByLatLng(lat, lng)
+                .then(({ district }) => {
+                    expect(district.districtCode)
+                        .to.eq('OH-12');
+                    done();
+                }).catch(done);
+        });
+
+        it('can find a district in Rancho Cucamonga, CA', (done) => {
+
+            const lat = 34.119695;
+            const lng = -117.5924517;
+
+            getDistrictByLatLng(lat, lng)
+                .then(({ district }) => {
+                    expect(district.districtCode)
+                        .to.eq('CA-31');
+                    done();
+                }).catch(done);
+        });
+    });
+
+    context('with a latititude and longitude outside the US', () => {
+
+        let spy;
+        const lat = 31.6538179;
+        const lng = -106.5890206;
+
+        beforeEach((done) => {
+            spy = sinon.spy(request, 'get');
+            done();
+        });
+
+        afterEach((done) => {
+            request.get.restore();
+            done();
+        });
+
+        it('rejects the promise with a helpful error', (done) => {
+            getDistrictByLatLng(lat, lng)
+                .then(() => {
+                    done(Error('Promise should be rejected.'))
+                })
+                .catch(({ statusCode, message }) => {
+                    expect(statusCode).to.eq(404);
+                    expect(message).to.eq(OUTSIDE_US(lat, lng, 'MX'));
+                    done();
+                })
+                .catch(done);
+        });
+
+        it('only makes a single http request', (done) => {
+            getDistrictByLatLng(lat, lng)
+                .then(() => {
+                    done(Error('Promise should be rejected.'))
+                })
+                .catch(() => {
+                    expect(spy.callCount).to.eq(1);
+                    done();
+                })
+                .catch(done);
+        });
+    });
+
+    context('with invalid lat lng coordinates', () => {
+
+        let spy;
+
+        beforeEach((done) => {
+            spy = sinon.spy(request, 'get');
+            done();
+        });
+
+        afterEach((done) => {
+            request.get.restore();
+            done();
+        });
+
+
+        it('throws an informative error', (done) => {
+            getDistrictByLatLng(null, null)
+                .then(() => {
+                    done(Error('Promise should be rejected.'))
+                })
+                .catch(({ message, statusCode }) => {
+                    expect(statusCode).to.eq(400);
+                    expect(message).to.eq(INVALID_REQUEST(null, null));
+                    done();
+                })
+                .catch(done);
+        });
+
+        it('doesn\'t make any http requests', (done) => {
+            getDistrictByLatLng()
+                .then(() => {
+                    done(Error('Promise should be rejected.'))
+                })
+                .catch(() => {
+                    expect(spy.callCount).to.eq(0);
+                    done();
+                })
+                .catch(done);
+        });
+    });
+});
